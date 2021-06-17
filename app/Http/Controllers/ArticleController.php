55098ccd8da2e2f4fa6c407fe3c74820a2e7cc79
ye.kulikov <Kulickov59@yandex.ru>
2021-06-17 18:48:22 +0300

    create article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,10 +7,21 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $articles = Article::paginate(5);
-        return view('article.index', compact('articles'));
+        $query = Article::query();
+        $userQuery = $request->input('q');
+
+        if ($userQuery) {
+            $query->where('name', 'like', "%{$userQuery}%");
+        }
+
+        $articles = $query->paginate();
+
+        return view('article.index', [
+            'articles' => $articles,
+            'query' => $userQuery,
+        ]);
     }
 
     public function show($id)
@@ -18,4 +29,35 @@ class ArticleController extends Controller
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
     }
+
+    // Вывод формы
+    public function create()
+    {
+        // Передаём в шаблон вновь созданный объект. Он нужен для вывода формы через Form::model
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    // Здесь нам понадобится объект запроса, для извлечения данных
+    public function store(Request $request)
+    {
+        // Проверка введенных данных
+        // Если будут ошибки, то возникнет исключение
+        $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:1000',
+        ]);
+
+        $article = new Article();
+        // Заполнение статьи данными из формы
+        $article->fill($request->all());
+        // При ошибках сохранения возникнет исключение
+        $article->save();
+
+        // Редирект на указанный маршрут с добавлением флеш сообщения
+        $request->session()->flash('message', 'Статья создана');
+        return redirect()
+            ->route('articles.index');
+      }
+
 }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Список статей</h1>
+    @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{session()->get('message')}}
+        </div>
+    @endif
+    <a href="{{route('articles.create')}}">Создать статью</a>
+    <h2>Поиск статей</h2>
+    {{Form::open(['url' => route('articles.index'), 'method' => 'GET'])}}
+        {{Form::text('q', $query)}}
+        {{Form::submit('Найти')}}
+    {{Form::close()}}
+    <h2>Список статей</h2>
     @foreach ($articles as $article)
-        <h2><a href="{{route('articles.show', $article->id, ['article' => $article])}}">{{$article->name}}</a></h2>
+        <h2><a href="{{route('articles.show', $article)}}">{{$article->name}}</a></h2>
         <div>{{Str::limit($article->body, 200)}}</div>
     @endforeach
 @endsection
